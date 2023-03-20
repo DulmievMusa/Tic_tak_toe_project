17d3a0f6be38cb3ff7c4297c586f23f34f354e36
Musa Dulmiev <musadulmiev@gmail.com>
2023-03-20 16:53:18 +0300

    creting games table columns

--- data/games.py
+++ data/games.py
@@ -5,13 +5,14 @@ from sqlalchemy_serializer import SerializerMixin
 
 
 class User(SqlAlchemyBase, SerializerMixin):
-    __tablename__ = 'games'
+    __tablename__ = 'games'  # game_id, players_ids, who_move, matrix, waitcount, start_time, winner
 
-    game_id = sqlalchemy.Column(sqlalchemy.String, index=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                               unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+    game_id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    players_ids = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    who_move = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    matrix = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    waitcount = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    start_time = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+    winner = sqlalchemy.Column(sqlalchemy.String, nullable=True)
