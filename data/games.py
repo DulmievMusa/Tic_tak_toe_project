4233014e993cea2e9ad5b97f699db84e85ecf0a1
Musa Dulmiev <musadulmiev@gmail.com>
2023-03-20 16:47:13 +0300

    creting games table...

---
+++ data/games.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class User(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'games'
+
+    game_id = sqlalchemy.Column(sqlalchemy.String, index=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                               unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
